9789ca80c98f47bfd93b9d35d431b7b1a20457dd
chyroc <chyroc@qq.com>
2021-03-10 20:56:36 +0800

    init

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/chyroc/otp-cli
+
+go 1.16
+
+require (
+	github.com/atotto/clipboard v0.1.4
+	github.com/urfave/cli/v2 v2.3.0
+	github.com/xlzd/gotp v0.0.0-20181030022105-c8557ba2c119
+)
---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/atotto/clipboard"
+	"github.com/urfave/cli/v2"
+	"github.com/xlzd/gotp"
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "otp-cli",
+		Usage: "generate otp client",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "secret",
+				Aliases: []string{"s"},
+				Usage:   "otp secret text",
+			},
+			&cli.StringFlag{
+				Name:    "secret-file",
+				Aliases: []string{"f"},
+				Usage:   "otp secret file",
+			},
+			&cli.BoolFlag{
+				Name:    "copy",
+				Aliases: []string{"c"},
+				Usage:   "copy to clipboard",
+			},
+			&cli.BoolFlag{
+				Name:    "quiet",
+				Aliases: []string{"q"},
+				Usage:   "not output to console",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			secret := c.String("secret")
+			secretFile := c.String("secret-file")
+			isCopy := c.Bool("copy")
+			isQuiet := c.Bool("quiet")
+			if secretFile != "" {
+				bs, err := ioutil.ReadFile(secretFile)
+				if err != nil {
+					return err
+				}
+				secret = string(bs)
+			}
+			if secret == "" {
+				return fmt.Errorf("secret is empty")
+			}
+
+			res, err := generate(secret)
+			if err != nil {
+				return err
+			}
+
+			if isCopy {
+				if err = clipboard.WriteAll(secret); err != nil {
+					return err
+				}
+			}
+
+			if !isQuiet {
+				fmt.Println(res)
+			}
+
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func generate(secret string) (s string, err error) {
+	defer func() {
+		e := recover()
+		if e != nil {
+			err = fmt.Errorf("%s", e)
+		}
+	}()
+	return gotp.NewDefaultTOTP(strings.ToUpper(strings.Replace(secret, " ", "", -1))).Now(), nil
+}
