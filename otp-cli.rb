5e7d7742f0a0ade413e1c2f004fa52e9e72f9755
chyroc <chyroc@qq.com>
2021-03-11 17:20:38 +0800

    fix

--- otp-cli.rb
+++ otp-cli.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
+
+# brew config for otp-cli
 class OtpCli < Formula
   desc "Tool for generate otp code in terminal"
   homepage "https://github.com/chyroc/otp-cli"
-
   url "https://github.com/chyroc/otp-cli/releases/download/v0.3.0/otp-cli-0.3.0.tar.gz"
   sha256 "b0c3a0afb4f886db32bcc12ed3d6eec985fe9cf4487e4db412557dfbdd5dcafe"
+  license "Apache-2.0"
   head "https://github.com/chyroc/otp-cli"
 
   depends_on "go" => :build
@@ -13,6 +16,13 @@ class OtpCli < Formula
   end
 
   test do
-    assert_equal "otp-cli version v0.3.0\n", shell_output("#{bin}/otp-cli version")
+    (testpath/"secret.txt").write "testsecret"
+
+    assert_equal "otp-cli version v#{pkg_version}\n", shell_output("#{bin}/otp-cli version")
+    from_string = shell_output("#{bin}/otp-cli -s testsecret")
+    assert_equal 7, from_string.length
+    from_file = shell_output("#{bin}/otp-cli -f secret.txt")
+    assert_equal 7, from_file.length
+    assert_equal from_string, from_file
   end
 end
